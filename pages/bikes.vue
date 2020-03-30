2ce88cf6771b0e11cfb7d7d1984de8568fd33b3b
klementj <klementj@gmail.com>
2020-03-30 22:36:10 +0200

    Create page tamplate

---
+++ pages/bikes.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Explore the bikes</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.1s ease-out;
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+  transform-origin: 50% 50%;
+}
+</style>
