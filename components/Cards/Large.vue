25b20a4109a1d725e3688a2f88bfacaf07dd8123
klementj <klementj@gmail.com>
2020-06-11 15:25:59 +0200

    WIP Begin work on large card

---
+++ components/Cards/Large.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- <h1>{{ getBikes }}</h1> -->
+    <h1>{{ bikes }}</h1>
+  </div>
+</template>
+
+<script>
+// import { mapActions } from 'vuex'
+
+export default {
+  name: 'LargeCard',
+
+  computed: {
+  // ...mapActions({
+  //   getBikes: 'bike/fetchAllBikes'
+  // }),
+    bikes () {
+      return this.$store.state.bike.bikes
+    }
+  }
+  // },
+
+  // mounted () {
+  //   this.$store.dispatch('bike/fetchAllBikes')
+  // }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/bikes.vue
+++ pages/bikes.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Explore the bikes</h1>
+    <LargeCard />
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae corrupti numquam, facere nisi pariatur ea.</p>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reprehenderit pariatur. Eum aperiam blanditiis perferendis. Laudantium cum adipisci, beatae laboriosam aspernatur quisquam quae obcaecati eos, sequi iste sit tempore dolores.</p>
@@ -23,8 +24,24 @@
 </template>
 
 <script>
+import LargeCard from '@/components/Cards/Large.vue'
 
 export default {
+
+  components: {
+    LargeCard
+  },
+
+  head () {
+    return {
+      title: 'Bicycle innovation lab - bikes'
+    }
+  },
+
+  async asyncData ({ store }) {
+    await store.dispatch('bike/fetchAllBikes')
+    await console.log('ASYNC FETCH')
+  }
 }
 </script>
 
