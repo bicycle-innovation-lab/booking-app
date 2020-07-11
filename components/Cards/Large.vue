42ac69f0da44cab5144e909535f08a3357082f0b
Klement Baastrup Johansen <kljo@campusvejle.dk>
2020-07-11 15:41:59 +0200

    Init large Card component

--- components/Cards/Large.vue
+++ components/Cards/Large.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <!-- <h1>{{ getBikes }}</h1> -->
-    <h1>{{ bikes }}</h1>
+    <!-- <pre>{{ bikes }}</pre> -->
+    <article v-for="(bike, index) in bikes" :key="index">
+      <nuxt-link :to="bike.Name" class="card">
+        <div class="card__image">
+          <img :src="'http://localhost:1337' + bike.Images[0].url" :alt="bike.Images[0].alt">
+        </div>
+        <!-- <p>{{ bike.Description }}</p> -->
+        <h3>{{ bike.Name }}</h3>
+        <!-- <p>{{ bike.Price }}</p> -->
+      </nuxt-link>
+    </article>
   </div>
 </template>
 
@@ -28,5 +38,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .card__image {
+    height: 130px;
+  }
 </style>
