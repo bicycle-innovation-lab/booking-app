81e24f180db43fc734ee07357cd063aabb995c27
Klement Baastrup Johansen <kljo@campusvejle.dk>
2020-08-02 14:43:07 +0200

    Add icon and styling to category labels

--- components/CategoryLabels.vue
+++ components/CategoryLabels.vue
@@ -1,7 +1,8 @@
 <template>
   <ul class="categories">
     <li v-for="category in categories" :key="category.id">
-      {{ category.name }}
+      <img :src="$config.axios.baseURL + category.icon.url" alt="">
+      <small>{{ category.name }}</small>
     </li>
   </ul>
 </template>
@@ -24,5 +25,26 @@ export default {
   list-style-type: none;
   padding: 0;
   display: flex;
+  gap: .8rem;
+
+  li {
+    display: flex;
+    align-items: center;
+    padding: .5rem .8rem;
+    border: 1px solid hsl(0, 0%, 92%);
+    border-radius: var(--border-radius);
+
+    img {
+      flex: 0 0;
+      width: 16px;
+      height: 16px;
+      margin-right: .4rem;
+      margin-top: 2px;
+    }
+
+    small {
+      flex: 1 0;
+    }
+  }
 }
 </style>
